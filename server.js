41dee9a9326b64293d0bf314985a9152cbf2c09b
roxy22788 <74971931+roxy22788@users.noreply.github.com>
2023-09-24 19:16:03 +0000

    Fazer o logout do usuário

--- server.js
+++ server.js
@@ -47,8 +47,17 @@ myDB(async client => {
     }
   );
 
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  })
+
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
     res.render('profile', {username: req.user.username});
+  })
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
   })
 
   passport.use(new LocalStrategy((username, password, done) => {
