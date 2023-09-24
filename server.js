d36b2c892b0d8d45f3b726df56030fe515f06674
roxy22788 <74971931+roxy22788@users.noreply.github.com>
2023-09-24 18:07:26 +0000

    Implementar a serialização de um usuário do Passport

--- server.js
+++ server.js
@@ -27,21 +27,32 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' }
-  );
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please log in'
+    });
+  });
+  
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  })
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  
+}).catch(err => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  })
 });
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-})
-
-passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  // });
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
