ae9bac24fdb3743b3bc9fb4d686afd1487fa7316
roxy22788 <74971931+roxy22788@users.noreply.github.com>
2023-09-24 19:28:01 +0000

    Registrar novos usuários

--- server.js
+++ server.js
@@ -35,10 +35,34 @@ myDB(async client => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please log in',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     })
   }
   );
+
+  app.route('/register')
+  .post((req, res, next) => {
+    myDataBase.findOne({username: req.body.username}, (err, user) => {
+      if (err) { next(err); }
+      else if (user) { res.redurect('/') }
+      else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        }, (err, doc) => {
+          if (err) { res.redirect('/') }
+          else {
+            next(null, doc.ops[0]);
+          }
+        })
+      }
+    })
+  }, 
+  passport.authenticate('local', { failureRedirect: '/' }),
+  (req, res, next) => {
+    res.redirect('/profile');
+  });
 
   app.route('/login').post(
     passport.authenticate('local', { failureRedirect: '/' }),
@@ -50,7 +74,7 @@ myDB(async client => {
   app.route('/logout').get((req, res) => {
     req.logout();
     res.redirect('/');
-  })
+  });
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
     res.render('profile', {username: req.user.username});
